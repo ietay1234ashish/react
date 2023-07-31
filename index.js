68dc0f464e0e7e7b0729e2288a5b55ce0619b837
ASHISH KUMAR <134280103+ietay1234ashish@users.noreply.github.com>
2023-08-01 01:26:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,163 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+import "./index.css"
+
+ReactDOM.render(
+    <>
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    <Card 
+    imgsrc = "https://nitmeghalaya.in/wp-content/uploads/2023/06/IMG-20230614-WA0006.jpg"
+    sname = "Gadar 2"
+    title = "Bollywood Origal Film"
+    link = "https://www.imdb.com/title/tt15441054/"
+    
+    />
+    <Card
+     imgsrc = "https://images.hdqwalls.com/wallpapers/mr-robot-4k-pz.jpg"
+     sname = "Mr.Robot"
+     title = "Hollywood Orignal Series"
+     link = "https://www.netflix.com/"
+    />
+    <Card
+     imgsrc = "https://i.ytimg.com/vi/dQ9AOqAIgDc/maxresdefault.jpg"
+     sname = "Mirzapur Season 3"
+     title = "Bollywood Orignal Series"
+     link = "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiEjOnuzrmAAxVc1xYFHS4KAwoYABAAGgJ0bA&ohost=www.google.co.in&cid=CAESauD2ybU86VMxUpm25rsthCsPXeSYa6rsKVdSV-Q7JUvxW5HAGCGl0QxML7jeXOcilCx2FRbPtAzXsUBJkjz7Qyp5CLAqUBWRHMNHu05cOL5TsU8aJFLFwBSHs5MfbBQpaxJd9_7_jXUyxeo&sig=AOD64_1Q-swwnRr-HD1GP3esy7Y65OSdaA&q&adurl&ved=2ahUKEwjUquDuzrmAAxWY_GEKHX1oAYUQ0Qx6BAgJEAE"
+    />
+    
+    </>,
+    document.getElementById("root")
+)
